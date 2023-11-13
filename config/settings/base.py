bcff2f89cd4ab2d87d3fe7c154772effcbde5687
Shavkatjon <odambaev.sh@gmail.com>
2023-11-14 00:10:00 +0500

    feat: added news, cart, library apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -84,7 +84,10 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "users",
     # Your stuff: custom apps go here
+    "news",
     "courses",
+    "library",
+    "cart",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
